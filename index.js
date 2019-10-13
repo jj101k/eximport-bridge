61160f4e539cf60e75547e3511e8282f8b488882
Jim Driscoll <jim.a.driscoll@gmail.com>
2019-10-13 13:54:12 +0100

    Revert "Just return a real promise instead"
    This reverts commit 87b765d815b39dce1becb7eb974ea3823b84a061.

--- index.js
+++ index.js
@@ -7,18 +7,22 @@ class EximportBridge extends Promise {
     static get bridge() {
         return new EximportBridge()
     }
-    constructor() {
-        let commit
-        super(resolve => commit = resolve)
-        this.commit = commit
-        /**
-         * @type {{[symbol: string]: *}}
-         */
-        this.ns = {}
+    constructor(f) {
+        if(f) {
+            super(f)
+        } else {
+            let commit
+            super(resolve => commit = resolve)
+            this.commit = commit
+            /**
+             * @type {{[symbol: string]: *}}
+             */
+            this.ns = {}
+        }
     }
     importer(f) {
         this.then(
-            ns => {f(ns); return Promise.resolve(ns)},
+            ns => {f(ns); return ns},
         )
     }
 }
