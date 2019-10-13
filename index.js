adfd863a8f0488372843753737e5ae1eb662e3be
Jim Driscoll <jim.a.driscoll@gmail.com>
2019-10-13 13:57:47 +0100

    Revert "Convert into a derivation of Promise"
    This reverts commit 61801a79f1a6bbcd61bb21dbdf2a61d9b50c6958.

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-class EximportBridge extends Promise {
+/**
+ * @typedef {(namespace: {[symbol: string]: *}) => *} importer
+ */
+
+class EximportBridge {
     /**
      * This is a convenience property so you can just do
      * `require("eximport-bridge").bridge` rather than `(const b =
@@ -8,18 +12,44 @@ class EximportBridge extends Promise {
         return new EximportBridge()
     }
     constructor() {
-        let commit
-        super(resolve => commit = resolve)
-        this.commit = commit
+        /** @type {?importer[]} */
+        this.importers = []
         /**
          * @type {{[symbol: string]: *}}
          */
         this.ns = {}
     }
+    /**
+     *
+     * @param {{[symbol: string]: *}} finished_exports
+     */
+    commit(finished_exports) {
+        Object.assign(this.ns, finished_exports)
+        for(const f of this.importers) {
+            f(this.ns)
+        }
+        this.importers = null
+    }
+    /**
+     * This adds an importer hook. Usually this would be added implicitly via
+     * eximport, typically in a pattern like:
+     *
+     * ```
+     *  var Foo = undefined;
+     *  require("foo").importer(ns => Foo = ns.Foo)
+     * ```
+     *
+     * Once the file has been completely loaded, this will run immediately;
+     * before that, it will queue up an importer instead.
+     *
+     * @param {importer} f
+     */
     importer(f) {
-        this.then(
-            ns => {f(ns); return ns},
-        )
+        if(this.importers) {
+            this.importers.push(f)
+        } else {
+            f(this.ns)
+        }
     }
 }
 
